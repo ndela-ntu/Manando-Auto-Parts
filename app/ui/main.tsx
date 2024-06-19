207a3191c2c2b4b56694fcc0b0cd308b1887ca1b
ndela-ntu <ntulilindelani4@gmail.com>
2024-06-19 14:37:25 +0200

    aligned text

--- app/ui/main.tsx
+++ app/ui/main.tsx
@@ -4,7 +4,7 @@ import CategoryCard from './category-card';
 export default function Main() {
   return (
     <div className='flex flex-col items-center justify-center px-10 py-5'>
-      <h3 className="text-lg pb-5 text-[#E8E9ED]">{`Car parts available (by category):`}</h3>
+      <h3 className="text-lg pb-5 text-[#E8E9ED] text-center">{`Car parts available (by category):`}</h3>
       <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 place-items-center gap-5'>
       {categories.map((category, i) => (
         <CategoryCard

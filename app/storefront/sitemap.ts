a0c10330337ab637acfd64d99ef315ffb083adb2
ndela-ntu <ntulilindelani4@gmail.com>
2024-07-03 22:11:22 +0200

    added connect mongo

--- app/storefront/sitemap.ts
+++ app/storefront/sitemap.ts
@@ -1,8 +1,10 @@
 import Product, { IProduct } from '@/app/models/product';
 import { MetadataRoute } from 'next';
+import connectMongo from '../utils/connect-mongo';
 
 export async function generateSitemaps() {
   // Fetch the total number of products and calculate the number of sitemaps needed
+  await connectMongo();
   const products: IProduct[] = await Product.find();
   const toReturn = products.map((product) => ({
     id: product.id.toString(),
@@ -15,6 +17,7 @@ export default async function sitemap({
 }: {
   id: number;
 }): Promise<MetadataRoute.Sitemap> {
+  await connectMongo();
   const products = await Product.find();
   return products.map((product) => ({
     url: `https://www.manando.co.za/storefront/${product._id.toString()}`,

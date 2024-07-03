7f24b1984b350538b8165ea301eabc31499e4a89
ndela-ntu <ntulilindelani4@gmail.com>
2024-07-03 14:01:26 +0200

    routing and contact details

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,23 +1,24 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function AcmeLogo() {
   return (
-    <div
-      className={`${lusitana.className}`}
-    >
-      <Image
-        src="/MAP_LOGO.png"
-        alt="Picture of the author"
-        sizes="100vw"
-        style={{
-          width: '100%',
-          height: 'auto',
-        }}
-        width={500}
-        height={300}
-      />
-    </div>
+    <Link href="/">
+      <div className={`${lusitana.className}`}>
+        <Image
+          src="/MAP_LOGO.png"
+          alt="Picture of the author"
+          sizes="100vw"
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+          width={500}
+          height={300}
+        />
+      </div>
+    </Link>
   );
 }
--- app/ui/contact-details.tsx
+++ app/ui/contact-details.tsx
@@ -3,7 +3,7 @@ export default function ContactDetails() {
     <div className="flex flex-col items-center justify-center py-2 bg-[#816C61] text-[#E8E9ED] text-xs">
       <h2 className="text-base font-semibold">Contact Details</h2>
       <h3>Phone Number: +27786020598</h3>
-      <h3>Email: ntulilindelani4@gmail.com</h3>
+      <h3>Email: manando.autoparts@gmail.com</h3>
       <h3 className="text-center ">Address: 48 Piketberg PI, Lenasia South, 1835</h3>
     </div>
   );
